096a16239751ca620db4fccd31655c0fea525050
Tim Perkins <code@taughz.dev>
2023-07-16 15:24:47 -0400

    Updated Doom config

--- emacs/doom_config/config.el
+++ emacs/doom_config/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Tim Perkins"
+      user-mail-address "code@taughz.dev")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -28,20 +28,39 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font (font-spec :family "DejaVu Sans Mono")
+      doom-variable-pitch-font (font-spec :family "DejaVu Sans"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-tomorrow-night)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+;; Limit the minibuffer or echo area to one line. For good measure, limit Eldoc
+;; to use one line as well. Full output can be shown with `M-x eldoc'.
+(setq max-mini-window-height 1
+      eldoc-echo-area-use-multiline-p nil)
+
+;; Don't automatically continue comments. Why would you want that?
+(setq +default-want-RET-continue-comments nil)
+
+;; Don't hide my strings. Why would you want that?!
+(advice-add #'+emacs-lisp-truncate-pin :override (lambda () ()))
+
+;; Don't confirm killing of processes. Not sure why this is so hard.
+(setq confirm-kill-processes nil)
+(defun config--inhibit-query-on-exit ()
+  (set-process-query-on-exit-flag (get-buffer-process (current-buffer)) nil))
+(add-hook 'comint-exec-hook #'config--inhibit-query-on-exit)
+(after! vterm (add-hook 'vterm-mode-hook #'config--inhibit-query-on-exit))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
+(setq org-directory "~/Org/")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -74,3 +93,48 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Better for multi-monitor coding
+(map! "C-x o" #'next-window-any-frame
+      "C-x C-o" #'previous-window-any-frame)
+
+;; Better home and end keys
+(map! [home] #'doom/backward-to-bol-or-indent
+      "C-a" #'doom/backward-to-bol-or-indent
+      [end] #'end-of-line
+      "C-e" #'end-of-line)
+
+;; Add alternate key for redo
+(map! (:after undo-fu
+       :map undo-fu-mode-map
+       "C-?" #'undo-fu-only-redo))
+
+;; Use function keys for common utilities
+(map! "<f1>" #'+vterm/toggle
+      "<f2>" #'+default/search-other-cwd
+      "<f3>" #'+default/search-project
+      "<f4>" nil
+      "<f5>" nil
+      "<f6>" #'magit-dispatch
+      "<f7>" #'magit-status
+      "<f8>" #'magit-blame-addition
+      "<f9>" #'magit-log-current
+      "<f10>" #'magit-log-buffer-file
+      "<f11>" nil
+      "<f12>" nil)
+
+;; Map the above again for VTerm
+(map! :after vterm
+      :map vterm-mode-map
+      "<f1>" #'+vterm/toggle
+      "<f2>" #'+default/search-other-cwd
+      "<f3>" #'+default/search-project
+      "<f4>" nil
+      "<f5>" nil
+      "<f6>" #'magit-dispatch
+      "<f7>" #'magit-status
+      "<f8>" #'magit-blame-addition
+      "<f9>" #'magit-log-current
+      "<f10>" #'magit-log-buffer-file
+      "<f11>" nil
+      "<f12>" nil)
--- emacs/doom_config/init.el
+++ emacs/doom_config/init.el
@@ -49,13 +49,13 @@
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
-       workspaces        ; tab emulation, persistence & separate workspaces
+       ;;workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
-       fold              ; (nigh) universal code folding
+       ;;(evil +everywhere); come to the dark side, we have cookies
+       ;;file-templates    ; auto-snippets for empty files
+       ;;fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
@@ -63,7 +63,7 @@
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
+       ;;snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
@@ -77,11 +77,11 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       ;;(spell +flyspell) ; tasing you for misspelling mispelling
+       (spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
@@ -95,7 +95,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp               ; M-x vscode
+       lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -115,13 +115,13 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       ;;(cc +lsp)         ; C > C++ == 1
+       (cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       ;;data              ; config/data formats
+       data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
        ;;elixir            ; erlang done right
@@ -140,7 +140,7 @@
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       ;;json              ; At least it ain't XML
+       json              ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
@@ -148,7 +148,7 @@
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
@@ -157,7 +157,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
+       python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -172,8 +172,8 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       web               ; the tubes
+       yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
@@ -191,4 +191,4 @@
 
        :config
        ;;literate
-       (default +bindings +smartparens))
+       (default +bindings))
