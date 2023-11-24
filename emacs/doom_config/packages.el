811dcdb70199ac111af2e311c2811b5a31119992
Tim Perkins <code@taughz.dev>
2023-11-24 11:03:22 -0500

    Updated Doom config

--- emacs/doom_config/packages.el
+++ emacs/doom_config/packages.el
@@ -48,3 +48,6 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+(package! buffer-move
+  :recipe (:host github :repo "taughz/buffer-move"))
