a80156440e243734682c7cad25933d4d68007df6
Tim Perkins <code@taughz.dev>
2024-01-17 16:08:22 -0500

    Add ROS container

---
+++ containers/ros/bashrc_ros.bash
@@ -0,0 +1,4 @@
+# Set up autocompletion, etc
+source "/usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash"
+source "/usr/share/colcon_cd/function/colcon_cd.sh"
+source "/usr/share/vcstool-completion/vcs.bash"
