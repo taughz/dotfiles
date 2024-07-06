27994cd77fc16bca43a9af57c5622e68434563a6
Tim Perkins <code@taughz.dev>
2024-07-06 13:56:40 -0400

    Minor fixes to the user container

---
+++ containers/user/fix_path.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Copyright (c) 2024 Tim Perkins
+
+set -o errexit
+set -o nounset
+set -o pipefail
+IFS=$'\n\t'
+
+if [ -z "${OLD_DEV_USER_HOME:-}" -o -z "${DEV_USER_HOME:-}" ]; then
+    echo "ERROR: Critical environment variables are not defined!" >&2
+    exit 1
+fi
+
+# Fix the path, replacing the old HOME directory
+export PATH="$(echo $PATH | sed "s;$OLD_DEV_USER_HOME;$DEV_USER_HOME;g")"
+
+exec "$@"
